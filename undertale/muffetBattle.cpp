bba443da2a8377e7a6d9622ca3dd57704cb890e9
유정 김 <k8463282@naver.com>
2021-07-01 03:37:16 +0900

    final muffet bullet image

--- undertale/muffetBattle.cpp
+++ undertale/muffetBattle.cpp
@@ -7,6 +7,7 @@ HRESULT muffetBattle::init()
 	ImageManager::GetInstance()->AddImage("muffet_croissant", L"머펫이미지/croissant.png");
 	ImageManager::GetInstance()->AddFrameImage("muffet_pet_down", L"머펫이미지/pet down.png", 4, 1);
 	ImageManager::GetInstance()->AddFrameImage("muffet_pet_side", L"머펫이미지/pet side.png", 2, 1);
+	ImageManager::GetInstance()->AddImage("muffet_spider", L"머펫이미지/spider.png");
 
 	return S_OK;
 }
