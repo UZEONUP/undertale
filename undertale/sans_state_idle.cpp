e0b6e3d3fb52945b4eb6de781958eed0c0810f6d
현진 박 <park9217@hanmail.net>
2021-07-02 09:04:29 +0900

    z

--- undertale/sans_state_idle.cpp
+++ undertale/sans_state_idle.cpp
@@ -15,30 +15,40 @@ sans_state * sans_state_idle::inputHandle(sansBattle * sansBattle)
 	{
 		//하트 위치 지정
 		sansBattle->getUI()->set_inGame_heart_y(sansBattle->getUI()->get_main_rect().bottom);
-		if (ui->getBattle_turn() == 0)
+		if (ui->getBattle_turn() == 0 || ui->getBattle_turn() == 6 ||
+			ui->getBattle_turn() == 12 || ui->getBattle_turn() == 18 ||
+			ui->getBattle_turn() == 24)
 		{
 			return new sans_state_attack1();
 		}
-		if (ui->getBattle_turn() == 1)
+		if (ui->getBattle_turn() == 1 || ui->getBattle_turn() == 7 ||
+			ui->getBattle_turn() == 13 || ui->getBattle_turn() == 19 || 
+			ui->getBattle_turn() == 25)
 		{
 			return new sans_state_attack2();
 		}
-		if (ui->getBattle_turn() == 2)
+		if (ui->getBattle_turn() == 2 || ui->getBattle_turn() == 8 ||
+			ui->getBattle_turn() == 14 || ui->getBattle_turn() == 20 || 
+			ui->getBattle_turn() == 26)
 		{
 			return new sans_state_attack3();
 		}
-		if (ui->getBattle_turn() == 3)
+		if (ui->getBattle_turn() == 3 || ui->getBattle_turn() == 9 ||
+			ui->getBattle_turn() == 15 || ui->getBattle_turn() == 21)
 		{
 			return new sans_state_attack4();
 		}
-		if (ui->getBattle_turn() == 4)
+		if (ui->getBattle_turn() == 4 || ui->getBattle_turn() == 10 ||
+			ui->getBattle_turn() == 16 || ui->getBattle_turn() == 22)
 		{
 			return new sans_state_attack5();
 		}
-		if (ui->getBattle_turn() == 5)
+		if (ui->getBattle_turn() == 5 || ui->getBattle_turn() == 11 ||
+			ui->getBattle_turn() == 17 || ui->getBattle_turn() == 23 ||
+			ui->getBattle_turn() == 27)
 		{
 			return new sans_state_attack6();
-		}
+		}    
 	}
 
 	return nullptr;
